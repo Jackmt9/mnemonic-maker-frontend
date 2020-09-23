ddfb3223690793319b5757cf76542c830bac46bb
jackmt9 <jackmt9@gmail.com>
2020-09-23 16:11:48 -0400

    Script tag arround matching phrase

--- src/Home.js
+++ src/Home.js
@@ -12,24 +12,37 @@ export default class Home extends React.Component {
   }
 
   handleSubmit = (e) => {
-    const results = document.getElementById('lyrics')
-    results.innerText = ''
+    const lyricsDiv = document.getElementById('lyrics')
+    lyricsDiv.innerText = ''
     e.preventDefault()
     const loading = document.createElement('img')
     loading.src = 'https://media.giphy.com/media/DY2ujmJHaO9Vu/giphy.gif'
-    results.append(loading)
+    lyricsDiv.append(loading)
     fetchMnemonic(this.state.query)
     .then(r => {
       console.log(r.response)
+      this.appendLyrics(r.response, lyricsDiv)
 
-      this.setState({
-        lyrics: r.response.lyrics
-      })
-      results.innerText = ''
-      results.innerText = this.state.lyrics
-      // console.log(this.state.lyrics)
+      // this.setState({
+      //   lyrics: r.response.lyrics
+      // })
+      // lyricsDiv.innerText = this.state.lyrics
       }
     )
+
+  }
+
+  appendLyrics = ({lyrics, matching_range}, lyricsDiv) => {
+    const parseRange = document.createRange();
+    const parse = Range.prototype.createContextualFragment.bind(parseRange);
+
+    let lyricsArray = lyrics.split(' ')
+    lyricsArray.splice(matching_range[1], 0, "</span>")
+    lyricsArray.splice(matching_range[0], 0, "<span class='matching-lyrics'>")
+    let lyricsWithSpan = lyricsArray.join(' ')
+  
+    console.log(lyricsArray)
+    lyricsDiv.innerHTML = parse(lyricsWithSpan)
 
   }
 
