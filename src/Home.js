c331f38f2107729c5205d537cad10fe9efa4d4c1
Sean Tarzy <seantarzy@gmail.com>
2020-10-12 17:13:58 -0400

    refresh

--- src/Home.js
+++ src/Home.js
@@ -1,56 +1,31 @@
 import React, { createElement } from "react";
 import { fetchMnemonic } from './services/utils'
+import Song from './components/Song'
 export default class Home extends React.Component {
   state = {
-      query: '',
+      query: 'e',
+      handleSearch: false
   }
-  
   handleChange = (e)=>{
     this.setState({[e.target.name]: e.target.value})
   }
-  
+  refreshPage = ()=>{
+    this.setState({ handleSearch: false});
+  }
+  renderSong = ()=>{
+  }
   handleSubmit = (e) => {
     e.preventDefault()
-    
-    fetchMnemonic(this.state.query)
-    .then(r => {
-      console.log(r)
-      if (r.error) {
-        let songDiv = document.getElementById('song')
-        songDiv.innerText = r.error
-      } else {
-        this.appendLyrics(r.song, r.matching_phrase)
-      }
-      }
-    ) 
-  }
-    
-  appendLyrics = (song, matchingPhrase) => {
-    let songDiv = document.getElementById('song')
-    songDiv.innerText = ''
-
-    let lyrics = document.createElement('p')
-
-    song.lyrics.split('\n').forEach(line => {
-      lyrics.innerHTML += line.replace(matchingPhrase, (match) => `<mark>${match}</mark>`)
-      lyrics.innerHTML += '<br/>'
-    })
-
-    let title = document.createElement('a')
-    title.innerText = song.full_title
-    title.href = song.url
-
-    let songImage = document.createElement('img')
-    songImage.src = song.image
-    songImage.alt = 'Album Cover'
-
-
-    songDiv.append(title, songImage, lyrics)
-  }
+    setTimeout( ()=> {
+        this.setState({ handleSearch: true });
+    }, 10);
+    this.refreshPage()
+    };
+     ;
 
   render() {
     return (
-      <>
+           <>
         <form onSubmit={this.handleSubmit}>
           <label>
             Enter Input:
@@ -64,7 +39,12 @@ export default class Home extends React.Component {
           <br />
           <input type="submit" value="Submit" />
         </form>
-        <div id="song"></div>
+        {this.state.handleSearch?
+        <Song query ={this.state.query}
+            handleSearch ={this.state.handleSearch}/>
+            :
+            null
+            }
       </>
     );
   }
