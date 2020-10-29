9e0889caeab3691b52241927f872f649b0e2e2e0
Sean Tarzy <seantarzy@gmail.com>
2020-10-29 17:06:15 -0400

    started to implement redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+let initialState = {
+  pets: [
+    {
+      id: 29,
+      name: "Heihei",
+      breed: "Chicken"
+    }
+  ]
+}
+
+
+// RETURN VALUE OF REDUCER BECOMES THE NEW STATE
+let reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case "ADD_ONE_PET":
+      let thePetToBeAdded = action.payload
+      let copyOfPets = [...state.pets, thePetToBeAdded]
+
+      return {
+        ...state,
+        pets: copyOfPets
+      }
+
+    default:
+      return state
+  }
+
+}
+
+let storeObj = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={storeObj}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
