cc8c13b8aaace85aa51d930b5ddc300ed3f811c5
jackmt9 <jackmt9@gmail.com>
2020-11-23 16:25:38 -0500

    logo container removed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import LinkedIn from '../assets/linkedin.png'
 export default class About extends React.Component {
     render(){
         return(
-            <p>Insert About Info Here</p>
+            <div className='about'>
+
+                <h2>About The Creators</h2>
+                <a href="https://www.linkedin.com/in/sean-tarzy-997076110/" target="_blank"><img src={LinkedIn} className="linkedIn-button" alt="Sean's LinkedIn"/></a>
+                <a href="https://www.linkedin.com/in/jackmt9/" target="_blank"><img src={LinkedIn} className="linkedIn-button" alt="Jack's LinkedIn"/></a>
+            </div>
         )
     }
 }
